4e617bbbe4c0ad1c034c397064eb2e46b8c75bc2
Shreya Randive <shreya@Shreyas-MacBook-Pro.local>
2019-04-27 23:05:23 -0400

    Segue to product vc, put data

--- CoderSwag/Controller/CategoriesVC.swift
+++ CoderSwag/Controller/CategoriesVC.swift
@@ -33,5 +33,21 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
             return CategoryCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productVC = segue.destination as? ProductVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productVC.initProducts(category: sender as! Category)
+        }
+    }
 }
 
