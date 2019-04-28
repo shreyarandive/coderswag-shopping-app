6386e0d8433b419b8abf5a28d9a45f5c7787bc0f
Shreya Randive <shreya@Shreyas-MacBook-Pro.local>
2019-04-27 20:24:50 -0400

    Add UICollectionView

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Shreya Randive on 4/27/19.
+//  Copyright © 2019 Shreya Randive. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel! 
+}
